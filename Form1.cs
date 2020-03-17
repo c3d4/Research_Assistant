757fe12aec7ec609d639dabb9e4403c5c0ac9927
c3d4 <40456035+c3d4@users.noreply.github.com>
2020-03-17 23:06:13 +0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using System.Threading;
 
 namespace FinalResearchAssistant
 {
@@ -18,6 +11,8 @@ namespace FinalResearchAssistant
         private bool _dragging; //Bool to check if mouse is being dragged
         private Point _startPoint = new Point(0, 0);    // Point of the cursor
         private int colorOffset = 30;   // The change in color from hovering over a GUI object
+
+        ResultsForm resultsForm =  new ResultsForm();
 
         #region GUI
         // Change the color of the close button
@@ -81,33 +76,47 @@ namespace FinalResearchAssistant
             // Create the merged file 
         }
 
+        // Reference the two web services
         ApiHelper arxiv = new ApiHelper();
         ApiHelper core = new ApiHelper();
 
         private void bunifuFlatButton1_Click_1(object sender, EventArgs e)
         {
-            string searchQuery = bunifuMaterialTextbox1.Text;
-            arxiv.url = "http://export.arxiv.org/api/query?search_query=" + searchQuery + "&max_results=10";
-            arxiv.GetRequest();
+            if (bunifuMaterialTextbox1.Text != "")
+            {
+                string searchQuery = bunifuMaterialTextbox1.Text;
+                arxiv.url = "http://export.arxiv.org/api/query?search_query=" + searchQuery + "&max_results=5";
+                arxiv.GetRequest();
 
-            core.url = "https://core.ac.uk:443/api-v2/journals/search/" + searchQuery + "? page=1&pageSize=10&apiKey=rnBoaFLv8jiJd6N3XzECwule9SR7MPmH";
-            core.GetRequest();
+                core.url = "https://core.ac.uk:443/api-v2/journals/search/" + searchQuery + "? page=1&pageSize=10&apiKey=rnBoaFLv8jiJd6N3XzECwule9SR7MPmH";
+                core.GetRequest();
 
-            timer.Start();
+                // Start searching for events
+                timer.Start();
+                timer1.Start();
+                
+                resultsForm.ShowDialog();  //Show the settings form  instead of the app
 
-            // Using the new text file, have a multi-dimensional list here that can put stuff in a new form (also new text file for history)
-
+            }
         }
 
         // Wait for the files to exist
         private void timer_Tick(object sender, EventArgs e)
         {
-            if (File.Exists("Research_Results_XML.txt") || File.Exists("Research_Results_JSON.txt"))
+            if (File.Exists("Research_Results_XML.txt") && File.Exists("Research_Results_JSON.txt"))
             {
                 LocalGatherer.mergeFiles();
                 timer.Stop();   // Stop checking for the files to run the code once
             }
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (File.Exists("Research_Results_Merged.txt"))
+            {
+                timer1.Stop();   // Stop checking for the files to run the code once
+            }
+        }
     }
 }
-
