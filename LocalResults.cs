bb769e3d6d5c8f8ccac720e5bde597c9f82d7c41
c3d4 <40456035+c3d4@users.noreply.github.com>
2020-03-22 18:34:25 +0400

    Update LocalResults.cs

--- LocalResults.cs
+++ LocalResults.cs
@@ -7,6 +7,7 @@ namespace FinalResearchAssistant
     public partial class LocalResults : Form
     {
         string query;
+        string user; 
 
         public LocalResults()
         {
@@ -14,9 +15,9 @@ namespace FinalResearchAssistant
         }
 
         // Get the query 
-        public void getQuery(string _query)
+        public void getUser(string username)
         {
-            query = _query;
+            user = username;
         }
 
         // Close the form 
@@ -26,10 +27,18 @@ namespace FinalResearchAssistant
             this.Close();
         }
 
-        private void LocalResults_Shown(object sender, EventArgs e)
+        // Linear Search Algorithm
+        private void searchButton_Click(object sender, EventArgs e)
         {
+            richTextBox1.Visible = true;
+            bunifuFlatButton2.Location = new System.Drawing.Point(524, 646);
+            bunifuFlatButton2.Size = new System.Drawing.Size(206, 56);
+
             // Linear Search Algorithm
-            string[] lines = File.ReadAllLines("Research_Results_History.txt");
+            string[] lines = File.ReadAllLines(@"" + user + @"\Research_Results_History.txt");
+            
+            query = bunifuMaterialTextbox1.Text;
+            
             int count = lines.Length;
 
             for (int i = 0; i < count; i++)
@@ -41,6 +50,24 @@ namespace FinalResearchAssistant
                 }
             }
             
+            if (richTextBox1.Text == "")
+            {
+                richTextBox1.AppendText("No Results Found!");
+            }
         }
+
+        // Close the form
+        private void bunifuFlatButton2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox1.Visible = false;
+            bunifuMaterialTextbox1.Text = "";
+            bunifuFlatButton2.Size = new System.Drawing.Size(129, 56);
+            bunifuFlatButton2.Location = new System.Drawing.Point(1104, 646);
+
+            this.Close();
+        }
+
+
     }
 }
