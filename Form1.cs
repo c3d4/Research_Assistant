96a12f2c46c2715e9c568ad06a07fbd86a3e2365
c3d4 <40456035+c3d4@users.noreply.github.com>
2020-03-22 18:32:13 +0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,42 +1,43 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
-using System.IO;
 
 namespace FinalResearchAssistant
 {
     public partial class Form1 : Form
     {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region GUI
+
         // Variables to move GUI 
         private bool _dragging; //Bool to check if mouse is being dragged
         private Point _startPoint = new Point(0, 0);    // Point of the cursor
         private int colorOffset = 30;   // The change in color from hovering over a GUI object
 
-        ResultsForm resultsForm =  new ResultsForm();
-        HistoryForm historyForm = new HistoryForm();
-        LocalResults localResults = new LocalResults();
-
-        #region GUI
         // Change the color of the close button
         private void label2_MouseEnter(object sender, EventArgs e)
         {
-            label2.BackColor = Color.FromArgb(72 + colorOffset, 73 + colorOffset, 114 + colorOffset);
+            label2.BackColor = Color.FromArgb(132 + colorOffset, 18 + colorOffset, 13 + colorOffset);
         }
 
         private void label2_MouseLeave(object sender, EventArgs e)
         {
-            label2.BackColor = Color.FromArgb(72, 73, 114);
+            label2.BackColor = Color.FromArgb(132, 18, 13);
         }
 
         // Change the color of the minimize button
         private void label3_MouseEnter(object sender, EventArgs e)
         {
-            label3.BackColor = Color.FromArgb(72 + colorOffset, 73 + colorOffset, 114 + colorOffset);
+            label3.BackColor = Color.FromArgb(132 + colorOffset, 18 + colorOffset, 13 + colorOffset);
         }
 
         private void label3_MouseLeave(object sender, EventArgs e)
         {
-            label3.BackColor = Color.FromArgb(72, 73, 114);
+            label3.BackColor = Color.FromArgb(132, 18, 13);
         }
 
         // Close the application when pressing the X button
@@ -69,76 +70,41 @@ namespace FinalResearchAssistant
             _dragging = true;
             _startPoint = new Point(e.X, e.Y);
         }
+
         #endregion
 
-        public Form1()
-        {
-            InitializeComponent();
+        #region Login and Signup
 
-            // Create the merged file 
+        // Reference Signup Form
+        Signup signupForm = new Signup();
+        Login loginForm = new Login();
+
+        // Signup Button
+        private void signupButton_Click(object sender, EventArgs e)
+        {
+            // Show the Signup form 
+            signupForm.ShowDialog();
         }
 
-        // Reference the two web services
-        ApiHelper arxiv = new ApiHelper();
-        ApiHelper core = new ApiHelper();
-
-        private void bunifuFlatButton1_Click_1(object sender, EventArgs e)
+        // Login Button
+        private void loginButton_Click(object sender, EventArgs e)
         {
-            if (bunifuMaterialTextbox1.Text != "")
-            {
-                string searchQuery = bunifuMaterialTextbox1.Text;
-                arxiv.url = "http://export.arxiv.org/api/query?search_query=" + searchQuery + "&max_results=5";
-                arxiv.GetRequest();
-
-                core.url = "https://core.ac.uk:443/api-v2/journals/search/" + searchQuery + "? page=1&pageSize=10&apiKey=rnBoaFLv8jiJd6N3XzECwule9SR7MPmH";
-                core.GetRequest();
-
-                // Start searching for events
-                timer.Start();
-                timer1.Start();
-                
-                resultsForm.ShowDialog();  //Show the settings form  instead of the app
-
-            }
+            // Show the login form
+            loginForm.ShowDialog();
         }
 
-        // Wait for the files to exist
-        private void timer_Tick(object sender, EventArgs e)
+        #endregion
+
+        #region Preferances 
+
+        // About science search
+        SettingsForm aboutForm = new SettingsForm();
+
+        private void preferancesButton_Click(object sender, EventArgs e)
         {
-            if (File.Exists("Research_Results_XML.txt") && File.Exists("Research_Results_JSON.txt"))
-            {
-                LocalGatherer.mergeFiles();
-                timer.Stop();   // Stop checking for the files to run the code once
-            }
+            aboutForm.ShowDialog();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-
-            if (File.Exists("Research_Results_Merged.txt"))
-            {
-                timer1.Stop();   // Stop checking for the files to run the code once
-            }
-        }
-
-        // Open the history form 
-        private void bunifuFlatButton4_Click(object sender, EventArgs e)
-        {
-            if (File.Exists("Research_Results_History.txt"))
-            {
-                historyForm.ShowDialog();
-            }
-        }
-
-        // Search locally 
-        private void bunifuFlatButton3_Click(object sender, EventArgs e)
-        {
-            if (bunifuMaterialTextbox1.Text != "")
-            {
-                localResults.getQuery(bunifuMaterialTextbox1.Text);
-
-                localResults.ShowDialog();
-            }
-        }
+        #endregion
     }
 }
